1f5af7b8352118d799201efd82c8b35477fda97d
Ayten Kocak <aytenkocak76@gmail.com>
2026-01-17 19:12:05 +0300

    git Add . git commit -m "Casslar tanımlandı" git push

---
+++ Core/CarBook.Domain/Entities/Banner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Banner
+    {
+        public int BannerID { get; set; }
+        public string Title { get; set; }
+        public string Describtion { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
