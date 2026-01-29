111c06a09cf37a484982f50810de63b598bc4f11
Ayten Kocak <aytenkocak76@gmail.com>
2026-01-30 01:40:03 +0300

    UI layout ve UILayout view component yapısı eklendi

---
+++ Frontend/CarBookWebCoreUI/ViewComponents/UILayout/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebCoreUI.ViewComponents.UILayout
+{
+    public class _HeadUILayoutComponentPartial :ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/CarBookWebCoreUI/ViewComponents/UILayout/_MainCoverUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebCoreUI.ViewComponents.UILayout
+{
+    public class _MainCoverUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
